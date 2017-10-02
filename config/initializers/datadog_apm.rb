b6e87c11ee71e3171d5550a6870432e5bd890a70
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-10-02 00:08:57 -0700

    [datadog] enable Dalli and SuckerPunch integrations; add Faraday integration to Octokit

--- config/initializers/datadog_apm.rb
+++ config/initializers/datadog_apm.rb
@@ -3,9 +3,11 @@
 
 if ENV.fetch('DATADOG_ENABLE_APM', 'false') != 'false'
   require 'ddtrace'
+  require 'sucker_punch'
 
   Rails.logger.info("Enabling Datadog APM tracer")
 
+  # Rails instrumentation
   Rails.configuration.datadog_trace = {
     auto_instrument: true,
     default_service: 'samson',
@@ -17,4 +19,17 @@ if ENV.fetch('DATADOG_ENABLE_APM', 'false') != 'false'
       project: 'samson'
     }
   }
+
+  # Dalli instrumentation
+  Datadog::Monkey.patch_module(:dalli)
+  pin = Datadog::Pin.get_from(::Dalli)
+  pin.service = 'samson-memcached'
+
+  # Faraday instrumentation
+  Datadog::Monkey.patch_module(:faraday)
+
+  # SuckerPunch instrumentation
+  Datadog::Monkey.patch_module(:sucker_punch)
+  pin = Datadog::Pin.get_from(::SuckerPunch)
+  pin.service = 'samson-queues'
 end
